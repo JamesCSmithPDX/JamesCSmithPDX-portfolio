1cd0515ff6414d43b75c06809f461454504c00c2
JamesCSmithPDX <james.c.smithpdx@gmail.com>
2016-08-09 15:51:42 -0700

    route error

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,9 +1,5 @@
 page('/', Project.fetchAll, projectView.initIndexPage);
-<<<<<<< HEAD
-page('/portfolio', Project.fetchAll);
-=======
 page('/portfolio', Project.fetchAll, projectView.initIndexPage, projectView.portfolio);
->>>>>>> staging
 page('/portfolio/*', '/portfolio');
 page('/about', projectView.about);
 page();
